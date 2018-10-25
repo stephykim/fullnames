e88b6e9dfdf656600441440bec547a87fe769a1c
stephykim <stephaniekim@Stephanies-Air.attlocal.net>
2018-10-24 22:10:01 -0500

    have search and searchhistory functional

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { DashboardService } from './dashboard.service';
 
 @Component({
@@ -8,8 +8,14 @@ import { DashboardService } from './dashboard.service';
 })
 export class DashboardComponent implements OnInit {
   searches: any[];
+  results: string;
+  @Input() firstNameSearch: string;
+  @Input() lastNameSearch: string;
+  
+
   constructor(private dashboardService: DashboardService) {
     this.searches = [];
+    this.results = "";
   }
 
   searchHistory() {
@@ -18,6 +24,30 @@ export class DashboardComponent implements OnInit {
     });
   }
 
+  searchName() {
+    this.dashboardService.searchFirstName(this.firstNameSearch).subscribe(params => {
+      if (params.key === this.firstNameSearch) {
+        console.log("we got a first name match " + params.key);
+
+        this.dashboardService.searchLastName(this.lastNameSearch).subscribe(params => {
+          if (params.key === this.lastNameSearch) {
+            console.log("we got a last name match " + params.key);
+            this.results = this.firstNameSearch + " " + this.lastNameSearch + " is a valid name"
+          }
+          else {
+            this.results = this.firstNameSearch + " " + this.lastNameSearch + " is not found"
+          }
+        });
+      }
+      this.searches.push(this.firstNameSearch + " " + this.lastNameSearch);
+    });
+  }
+
+  addName() {
+    console.log("before");
+    this.dashboardService.addName(this.firstNameSearch, this.lastNameSearch);
+    console.log("after");
+  }
   ngOnInit() {
   }
 
